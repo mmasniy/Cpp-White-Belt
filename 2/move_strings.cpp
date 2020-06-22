e87aee097ccb1eded477fc518b13f9bc81e58558
Maksim Masniy <mmasniy@e1r3p8.unit.ua>
2020-06-22 19:02:24 +0300

    comment

--- 2/move_strings.cpp
+++ 2/move_strings.cpp
@@ -10,15 +10,15 @@ void MoveStrings(vector<string>& source, vector<string>& destination) {
 	source.clear();
 }
 
-// int main() {
-// 	vector<string> a = {"a", "b", "c"};
-// 	vector<string> b = {"y", "x", "z"};
+int main() {
+	vector<string> a = {"a", "b", "c"};
+	vector<string> b = {"y", "x", "z"};
 
-// 	MoveStrings(a, b);
-// 	if (a.empty()) {
-// 		cout << "a is empty" << endl;
-// 	}
-// 	for (auto i : b)
-// 		cout << i << endl;
-// 	return 0;
-// }
+	MoveStrings(a, b);
+	if (a.empty()) {
+		cout << "a is empty" << endl;
+	}
+	for (auto i : b)
+		cout << i << endl;
+	return 0;
+}
